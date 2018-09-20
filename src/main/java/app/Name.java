8a2017f1c62659bba0537247a80bc219f4ec08fd
Chuyang Chen <cdavidnz@gmail.com>
2018-09-20 23:03:29 +1200

    Name class now stores all versions of a single name when creating the name object

--- src/main/java/app/Name.java
+++ src/main/java/app/Name.java
@@ -1,60 +1,42 @@
 package app;
 
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.filefilter.WildcardFileFilter;
 
 import java.io.File;
+import java.io.FileFilter;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class Name {
 
-    File _file;
+    List<File> _files;
     private String _fullName;
     private String _name;
-    private String _date;
 
     public Name(File file){
-        _file = file;
-        getFileName();
-        getNameDate();
+        getFileName(file);
+        getAllFilesOfName();
     }
 
+    private void getFileName(File file) {
 
-    /**
-     * generates a human readable date from creation title ONLY WORKS ON NON-COMBINED CREATIONS
-     * @return String representing date: d-m-y h:m:s
-     */
-    public void getNameDate() {
+        String displayName = file.getName();
 
-        String processedDate = "";
-
-        String displayDate = _fullName;
-        displayDate = displayDate.replaceAll("^[^_]*_", "");
-        displayDate = displayDate.replaceAll("[^\\d_-]", "");
-        displayDate = displayDate.replaceAll("[.][^.]+$", "");
-        displayDate = displayDate.replaceAll("_", " ");
-        String[] dateAndTime = displayDate.split(" ", 2);
-        dateAndTime[1] = dateAndTime[1].replaceAll("-", ":");
-
-        processedDate = dateAndTime[0] + " " + dateAndTime[1];
-
-        if (!processedDate.equals("")){
-            _date = processedDate;
-        } else {
-            throw new IllegalArgumentException("Something went wrong with processing the file's date");
-        }
-    }
-
-    private void getFileName() {
-
-        String displayName = _file.getName();
         _fullName = displayName;
+
         displayName = displayName.replaceAll("^[^_]*_[^_]*_[^_]*_", "");
         displayName = displayName.replaceAll("[.][^.]+$", "");
 
         _name = displayName;
     }
 
+    /**
+     * checks if the file provided for the name is a valid .wav file
+     * @return true if te file is valid
+     */
     public boolean isValid() {
 
         if (!FilenameUtils.getExtension(_fullName).equals("wav")) {
@@ -64,16 +46,17 @@ public class Name {
         return true;
     }
 
+    private void getAllFilesOfName(){
+        //TODO get all files that include that name
+        File dir = new File(NameSayer.creationsPath);
+        FileFilter filter = new WildcardFileFilter("*"+_name+".wav");
+        File[] files = dir.listFiles(filter);
+        _files = Arrays.asList(files);
+
+    }
 
     public String getName(){
         return _name;
     }
 
-    public String getFullName(){
-        return _fullName;
-    }
-
-    public File getFile(){
-        return _file;
-    }
 }
