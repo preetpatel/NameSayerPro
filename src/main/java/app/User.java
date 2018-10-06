c0f34defe7d12e265d55f54045a7560851004b6a
Chuyang Chen <cdavidnz@gmail.com>
2018-10-06 20:08:12 +1300

    User object will now read scores off the score.txt file in the NameSayer directory

--- src/main/java/app/User.java
+++ src/main/java/app/User.java
@@ -1,17 +1,19 @@
 package app;
 
+import java.io.*;
+
 public class User {
 
     private String _username;
     private String _name;
     private int _score;
 
-    public User(String username) {
+    public User(String username) throws FileNotFoundException, IOException {
         _username = username;
 
         // TODO Need to change this to reflect database connection
         _name = "Demo";
-        _score = 0;
+        readScores();
     }
 
     public String getName() {
@@ -24,5 +26,20 @@ public class User {
 
     public String getUsername() {
         return _username;
+    }
+
+    private void readScores() throws FileNotFoundException, IOException {
+        _score = 0;
+        BufferedReader br = new BufferedReader(new FileReader(NameSayer.directoryPath +"/score.txt"));
+        String line;
+
+        while ((line = br.readLine())!=null) {
+            String[] scoreInfo = line.split("\\s+");
+            if (scoreInfo[0].equals(_username)){
+                _score = Integer.parseInt(scoreInfo[1]);
+            }
+        }
+
+
     }
 }
